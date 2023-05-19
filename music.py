63d7e9e34a80639e483d84061b792ee839a5b328
Jesus Ramos <125627643+JesusRms1@users.noreply.github.com>
2023-05-18 17:24:47 -0700

    Update music.py
    Directory works, play, pause, next and prev button work

--- music.py
+++ music.py
@@ -11,32 +11,42 @@ root.title('Music player')
 #creates the size of the window
 root.geometry("500x300")
 #window backgroung color
-
+# bg = PhotoImage(file="music.png")
+# # Create Canvas
+# canvas1 = Canvas( root, width = 400,
+#                  height = 400)
+  
+# canvas1.pack(fill = "both", expand = True)
+  
+# # Display image
+# canvas1.create_image( 0, 0, image = bg, 
+#                      anchor = "nw")
 
 root.resizable(False, False)
-mixer.init()
+pygame.mixer.init()
 
 menubar = Menu(root)
 root.config(menu=menubar)
 
-songs = []
+playlist = []
 current_song = ""
-pause = False
+paused = False
 
 def load_music():
     global current_song
+    
     root.directory = filedialog.askdirectory()
     
     for song in os.listdir(root.directory):
-        name, ext = os.path.splitext(song)
+        name,ext = os.path.splitext(song)
         if ext == '.mp3':
-            songs.append(song)
+            playlist.append(song)
     
-    for song in songs:
-        songs.insert("End", song)
+    for song in playlist:
+        songlist.insert("end", song)
     
     songlist.selection_set(0)
-    current_song = songs[songlist.curselection()[0]]
+    current_song = playlist[songlist.curselection()[0]]
 
 
 def play_music():
@@ -44,42 +54,44 @@ def play_music():
 
     if not paused:
         pygame.mixer.music.load(os.path.join(root.directory, current_song))
-        pygame.mixer.play()
+        pygame.mixer.music.play()
     else:
-        pygame.mixer.unpause()
+        pygame.mixer.music.unpause()
         paused = False
     
 
 def pause_music():
-    global pause
+    global paused
     pygame.mixer.music.pause()
-    pause = True
+    paused = True
 
 def next_music():
     global current_song, paused
+    
     try:
         songlist.select_clear(0, END)
-        songlist.selection_set(songs.index(current_song)+ 1)
-        current_song = songs(songlist.curselection()[0])
+        songlist.selection_set(playlist.index(current_song)+ 1)
+        current_song = playlist[songlist.curselection()[0]]
         play_music()
     except:
         pass
 
 def previous_music():
     global current_song, paused
+    
     try:
         songlist.select_clear(0, END)
-        songlist.selection_set(songs.index(current_song) - 1)
-        current_song = songs(songlist.curselection()[0])
+        songlist.selection_set(playlist.index(current_song) - 1)
+        current_song = playlist[songlist.curselection()[0]]
         play_music()
     except:
         pass
 
 orginize_menu = Menu(menubar, tearoff=False)
-orginize_menu.add_command(label='Music Folder',command=load_music)
-menubar.add_cascade(label='Music', menu= orginize_menu)
+orginize_menu.add_command(label='Music',command=load_music)
+menubar.add_cascade(label='Music Folder', menu= orginize_menu)
 
-songlist = Listbox(root, bg="black", fg="white", width=100, height=15)
+songlist = Listbox(root,bg = "black", fg="white", width=100, height=15)
 songlist.pack()
 
 play_btn_image = PhotoImage(file='play.png')
@@ -101,4 +113,6 @@ pause_btn.grid(row=0, column=2,padx=7,pady=10)
 next_btn.grid(row=0, column=3,padx=7,pady=10)
 prev_btn.grid(row=0, column=0,padx=7,pady=10)
 
+
 mainloop()
+
